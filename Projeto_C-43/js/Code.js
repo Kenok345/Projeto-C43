eae5f5dc2843c4ffdf2ad0b61675aadd3204b94a
Kenok345 <94133289+Kenok345@users.noreply.github.com>
2022-10-10 15:26:49 -0300

    Add files via upload

---
+++ Projeto_C-43/js/Code.js
@@ -0,0 +1,21 @@
+const accessCode1 = "VARIÁVEL";
+const accessCode2 = "FUNÇÃO";
+const accessCode3 = "BANCO DE DADOS";
+
+function clues() {
+  fill("white");
+  textSize(15);
+  fill("lightblue");
+  text("Dica: Sempre mudando, não constante!", 100, 70);
+
+  // Adicione código para exibir as outras duas perguntas.
+  fill("white");
+  textSize(15);
+  fill("lightblue");
+  text("Dica: A #$**!@ de x é ax. Use matemática.", 700, 170);
+
+  fill("white");
+  textSize(15);
+  fill("lightblue");
+  text("Dica: Onde você senta e tem um monte de cubos com números", 100, 270);
+}
